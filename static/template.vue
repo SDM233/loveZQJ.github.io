f92079b2280dd1ccab3e6caef8e214d576f8f016
Reborn <s15021209145@sina.com>
2017-04-28 10:59:54 +0800

    v2.2

---
+++ static/template.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+      return {
+        msg: 'template Vue',
+        name: 'Reborn',
+      }
+    },
+  components: {
+
+  },
+  mounted() {
+
+  },
+  methods:{
+    foo() {
+      console.log('foo')
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
